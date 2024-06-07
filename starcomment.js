412276acf876ca5dd2d87bc5ed5c335547b7e273
catborn <itnoz@yandex.ru>
2024-06-07 23:04:16 +0300

    feedback 2 of 5

--- starcomment.js
+++ starcomment.js
@@ -9,13 +9,21 @@ let resultComment = document.querySelector("#resultComment");
 const checkSpam = () => {
   // Проверка имени
   let lowerName = userName.value.toLowerCase();
-  resultName.textContent = lowerName[0].toUpperCase() + lowerName.slice(1);
+  let trimName = lowerName.trim();
+  // console.log(trimName);
+  resultName.textContent = trimName[0].toUpperCase() + trimName.slice(1);
   //ссылка на аватар, пример ссылки на аватар = "https://cs13.pikabu.ru/post_img/big/2023/09/11/5/1694416670162565263.jpg"
   resultPicture.src = picture.value;
   // сообщение пользователя с проверкой 'buy ViAgRA now' и 'free xxx'
   const commentSpam = comment.value.toLowerCase();
-  reComment = commentSpam.replace("xxx", "***").replace("viagra", "***");
+  reComment = commentSpam.replace(/xxx|viagra/g, "***");
   resultComment.textContent = reComment;
 };
+
+// + нужно было убрать лишние пробелы в начале и в конце, если они есть, можно просто метод trim() использовать
+// - checkSpam(str) должно было быть отдельной функцией именно для проверки комментария,
+// + проверку можно было сделать в одном методе commentSpam.replace(/xxx|viagra/g, "***")
+// - по-хорошему было бы отчистить все поля в форме
+// - нет возможности добавить новый комментарий: по сути у тебя функция должна была бы создавать новый <div> с комментарием и добавлять его в твой <fieldset>
 
 button.addEventListener("click", checkSpam);
